015aefd460b916425622f26a2f9d0039efe79351
Ramu4r4g <raguram9024@gmail.com>
2025-06-29 20:25:58 +0530

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -4,10 +4,10 @@ var isOdd = (num) => {
     }
     return false;
 }
-console.log(isOdd(13)); 
-const numbers1 = [13, 15, 17, 19];
-const result1 = numbers1.every(isOdd);
-console.log(result1);
+console.log(isOdd(13));
+const num1 = [13, 15, 17, 19];
+const res1 = num1.every(isOdd);
+console.log(res1);
 var result2 = [3, 5, 7, 9].every((n) => {
     return n % 2 !== 0;
 });
